6800f6128c7a0aa4ba86265c58ca17fb7573ac2f
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-01-04 20:54:59 -0300

    criando o banco de dados

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', (req, res) => {
-  return res.status(200).send({message: 'teste'})
+app.get('/ads', (request, response) => {
+  return response.status(200).send({message: 'teste'})
 })
 
 app.listen(3333)
