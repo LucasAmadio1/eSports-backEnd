9edbd089030e11834febb1d8a2b7d062b3e87718
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-01-06 16:56:18 -0300

    pasta de rotas

--- src/server.ts
+++ src/server.ts
@@ -1,98 +1,15 @@
 import express from 'express'
 import cors from 'cors'
-import { prisma } from './lib/prisma'
-import { convertHoursStringToMinutes } from './utils/convert-hour-string-to-minutes'
-import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string'
 
-const app = express()
+import { Routes } from './routes/routes'
+
+export const app = express()
+
+app.use(Routes)
 
 app.use(express.json())
 
 app.use(cors())
 
-app.get('/games', async (request, response) => {
-  const games = await prisma.game.findMany({
-    include: {
-      _count: {
-        select: {
-          ads: true
-        }
-      }
-    }
-  })
-
-  return response.json(games)
-})
-
-app.get('/games/:id/ads', async (request, response) => {
-  const gameId = request.params.id
-
-  const ads = await prisma.ad.findMany({
-    select: {
-      id: true,
-      name: true,
-      weekDays: true,
-      useVoiceChannel: true,
-      yearsPlaying: true,
-      hourStart: true,
-      hourEnd: true
-    },
-    where: {
-      gameId
-    },
-    orderBy: {
-      createdAt: 'desc'
-    }
-  })
-
-  return response.json(ads.map(ad => {
-    return {
-      ...ad,
-      weekDays: ad.weekDays.split(','),
-      hourStart: convertMinutesToHourString(ad.hourStart),
-      hourEnd: convertMinutesToHourString(ad.hourEnd)
-    }
-  }))
-})
-
-
-app.get('/ads/:id/discord', async (request, response) => {
-  const adId = request.params.id
-
-  const ad = await prisma.ad.findUnique({
-    select: {
-      discord: true,
-    },
-    where: {
-      id: adId
-    }
-  })
-
-  if (!ad) {
-    return response.status(400).send('Nenhum anúncio encontrado')
-  }
-
-  return response.json({discord: ad.discord})
-})
-
-app.post('/games/:id/ads', async (request, response) => {
-  const gameId = request.params.id
-  const body = request.body
-
-  const ad = await prisma.ad.create({
-    data: {
-      gameId,
-      name: body.name,
-      yearsPlaying: body.yearsPlaying,
-      discord: body.discord,
-      weekDays: body.weekDays.join(','),
-      hourStart: convertHoursStringToMinutes(body.hourStart),
-      hourEnd: convertHoursStringToMinutes(body.hourEnd),
-      useVoiceChannel: body.useVoiceChannel
-    }
-  })
-
-  return response.status(201).json(ad)
-})
 
 app.listen(3333)
